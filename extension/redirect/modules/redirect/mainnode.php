7548b69f67b164f9e0fa597fe871939a8c99631d
Kristof Coomans <kristof.coomans@telenet.be>
2006-02-28 10:38:35 +0000

    - Redirect extension, containing a redirect module with mainnode view which redirects to the main node of an object by specifying the object id

---
+++ extension/redirect/modules/redirect/mainnode.php
@@ -0,0 +1,32 @@
+<?php
+
+    $Module =& $Params['Module'];
+    $ObjectID =& $Params['ObjectID'];
+    $userParams =& $Params['UserParameters'];
+
+    include_once( 'kernel/classes/ezcontentobject.php' );
+
+    $object =& eZContentObject::fetch( $ObjectID );
+
+    if ( $object )
+    {
+        $mainNodeID =& $object->attribute( 'main_node_id' );
+
+        $userParamsString = '';
+
+        if ( count( $userParams ) > 0 )
+        {
+            foreach( $userParams as $key => $value )
+            {
+                $userParamsString = $userParamsString . '(' . $key . ')' . '/' . $value . '/';
+            }
+        }
+
+        return $Module->redirectTo( '/content/view/full/' . $mainNodeID . '/' . $userParamsString );
+    }
+    else
+    {
+        return $Module->handleError( EZ_ERROR_KERNEL_NOT_AVAILABLE, 'kernel' );
+    }
+
+?>
