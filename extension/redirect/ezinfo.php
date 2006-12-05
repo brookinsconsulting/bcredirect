020c3f88d1669f006bde5c48fe4374d079bce045
Kristof Coomans <kristof.coomans@telenet.be>
2006-12-05 13:11:54 +0000

    extension info file for eZ publish 3.9

---
+++ extension/redirect/ezinfo.php
@@ -0,0 +1,15 @@
+<?php
+class redirectInfo
+{
+    function info()
+    {
+        return array(
+            'Name' => "Redirect",
+            'Version' => "1.0",
+            'Copyright' => "Copyright (C) 2005-2006 SCK-CEN",
+            'Author' => "Kristof Coomans",
+            'License' => "GNU General Public License v2.0"
+        );
+    }
+}
+?>
